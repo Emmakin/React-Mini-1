71a5ceda52f908dc6f53050e77db645fbaa20d63
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-02 20:40:16 +0100

    Update App.js
    linking the secondary pop menu with the primary pop menu

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function DropdownMenu() {
 
   function DropdownItem(props) {
       return (
-        <a href="#" className="menu-item">
+        <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
           <span className="icon-button">{props.leftIcon}</span>
 
           {props.children}
